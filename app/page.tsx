b3a23a14bd3ca2a981293a6e462397723831f6c5
tw1ttt3r <tw1ttt3r@gmail.com>
2023-05-10 21:01:50 -0600

    feat: implemented new version lib-tw library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Hello } from 'lib-tw'
+import { Button, Message } from "lib-tw";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Hello name="peter"></Hello>
+      <Message message="Perer" />
     </main>
   )
 }
